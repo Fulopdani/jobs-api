4f19494abe6004a485232f5c94ee1e971f04068f
Dani <h259372@stud.u-szeed.hu>
2025-10-18 19:50:09 +0200

    initial commit

---
+++ db/connect.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+//6os mongooseban true, 7esben mar false lesz es warningot dobott
+mongoose.set('strictQuery', false);
+
+
+const connectDB = (url) => {
+  return mongoose
+  .connect(url)
+  .then(() => console.log('CONNECTED TO THE DB...'))
+  .catch((err) => console.log(err));
+}
+
+module.exports = connectDB;
